5233162e8fdd41614527fe5f5195ecb23e891dda
Hamzah <82611798+brobro10000@users.noreply.github.com>
2021-06-30 12:49:14 -0400

    refactored addID function and get

--- routes/apiRoutes/databaseRoutes.js
+++ routes/apiRoutes/databaseRoutes.js
@@ -1,13 +1,10 @@
 const router = require('express').Router();
 const { notes } =  require('../../data/notes')
 const { addID } = require('../../lib/id')
+
 router.get('/notes', (req, res) => {
-    for(let i = 0;i<notes.length;i++)
-    if(notes[i].id == undefined)
-    {
-      notes[i].id = addID()
-    }
-    res.json(notes)
+    let results = addID(notes)
+    res.json(results)
   });  
 
   module.exports = router;
