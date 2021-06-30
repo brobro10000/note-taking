8b16322e3c67316d97ba7b23c09f5daa6dde9a47
Hamzah <82611798+brobro10000@users.noreply.github.com>
2021-06-30 12:36:08 -0400

    created function to add id onto notes dynamically

--- routes/apiRoutes/databaseRoutes.js
+++ routes/apiRoutes/databaseRoutes.js
@@ -1,11 +1,13 @@
 const router = require('express').Router();
 const { notes } =  require('../../data/notes')
-
+const { addID } = require('../../lib/id')
 router.get('/notes', (req, res) => {
-    let results = notes
-    console.log(results)
-    console.log(notes)
-    res.json(results)
-  });
+    for(let i = 0;i<notes.length;i++)
+    if(notes[i].id == undefined)
+    {
+      notes[i].id = addID()
+    }
+    res.json(notes)
+  });  
 
   module.exports = router;
