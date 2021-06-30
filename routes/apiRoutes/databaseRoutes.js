bf31b19ee3b41ae4f01d05c03ece63514442f354
Hamzah <82611798+brobro10000@users.noreply.github.com>
2021-06-30 14:44:48 -0400

    First Autorefactor before heroku push
    Assignment at this point is completed w/ bonus

--- routes/apiRoutes/databaseRoutes.js
+++ routes/apiRoutes/databaseRoutes.js
@@ -1,19 +1,20 @@
 const router = require('express').Router();
-const { addID , deleteByID } = require('../../lib/id')
-var { notes } =  require('../../data/notes')
+const { addID, deleteByID } = require('../../lib/id')
+var { notes } = require('../../data/notes')
 
 router.get('/notes', (req, res) => {
-    let results = addID(notes)
-    res.json(results)
-  });  
+  let results = addID(notes)
+  res.json(results)
+});
 
 router.post('/notes', (req, res) => {
   let results = notes.push(req.body)
   res.json(results)
 });
-  module.exports = router;
 
-router.delete('/notes/:id', (req,res)=> {
+router.delete('/notes/:id', (req, res) => {
   notes = deleteByID(notes, req.params.id)
   res.json(notes)
 })
+
+module.exports = router;
--- server.js
+++ server.js
@@ -13,6 +13,6 @@ app.use(express.static('public'));
 app.use('/api', apiRoutes);
 app.use('/', htmlRoutes);
 
-app.listen(PORT,() => {
+app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
 })
